7c1aa3c20d85dd979ccfc38c94f66f7c0d98a5a3
Loch32 <loch932@gmail.com>
2023-08-10 22:10:19 +0800

    Create converter (0.1).py
    first iteration blegh code that probably makes no sense. !!!! WORK IN PROGRESS !!! i will most likely place all versions of my code in the repository for documentation (and storage space)

---
+++ converter (0.1).py
@@ -0,0 +1,22 @@
+#this first test build won't be using the api because i am dumb and haven't figured that bit out yet
+#and i'm doing this way later than i should've
+
+##### CODE SHAMELESSLY STOLEN FROM MR GIBBS START #####
+exchange_rates = [
+    {"currency": "AUD", "rate": 1.0},
+    {"currency": "EUR", "rate": 0.85},
+    {"currency": "GBP", "rate": 0.75},
+    {"currency": "USD", "rate": 0.60},]
+##### MCODE SHAMELESSLY STOLEN FROM MR GIBBS END #####
+
+IntCurrency = input("Please choose the initial currency, using a three-letter ISO 4217 currency code: ")
+
+#ensuring that there's no confusion on how to format the country code
+
+if IntCurrency == "AUD":
+        IntCurAmount = int(input("Please enter the initial currency amount as a decimal: "))
+else: print("Please try a different country code. At the moment, this converter only supports AUD, EUR, GBP, and USD.")
+
+TargCurrency = input("Please choose the target currency, using a three-letter ISO 4217 currency code: ")
+
+if TargCurrency == "EUR":
