ceb5dd564ce88bd01d504322b79165ffd8d21540
Loch32 <loch932@gmail.com>
2023-08-15 13:33:55 +0800

    Create converter (0.2).py
    v0.2

---
+++ converter (0.2).py
@@ -0,0 +1,12 @@
+#this is the second test build and also does not use the API
+
+IntCurrency = input("Please choose the initial currency, using a three-letter ISO 4217 currency code. If you are unsure of what code your currency is, type 'help': ")
+
+#ensuring that there's no confusion on how to format the country code
+
+if IntCurrency == "AUD":
+        IntCurAmount = int(input("Please enter the initial currency amount as a decimal: "))
+elif IntCurrency == "help".lower: print("https://en.wikipedia.org/wiki/ISO_4217#List_of_ISO_4217_currency_codes")
+else: print("Please try a different country code. At the moment, this converter only supports AUD, EUR, GBP, and USD.")
+
+TargCurrency = input("Please choose the target currency, using a three-letter ISO 4217 currency code: ")
