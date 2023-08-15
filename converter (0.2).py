03c555e9eae77eed75bd223acd663fcb6598faf1
Loch32 <loch932@gmail.com>
2023-08-15 14:08:01 +0800

    Update converter (0.2).py

--- converter (0.2).py
+++ converter (0.2).py
@@ -13,5 +13,5 @@ else: print("Please try a different country code. At the moment, this converter 
 TargCurrency = input("Please choose the target currency, using a three-letter ISO 4217 currency code: ")
 if TargCurrency == "GBP":
       result = IntCurAmount * 0.65
-      print(IntCurAmount, IntCurrency, "is equal to ", result, TargCurrency)
+      print(IntCurAmount, IntCurrency, "is equal to", result, TargCurrency)
 else: print("Please try again.")   
