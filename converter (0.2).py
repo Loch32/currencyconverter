6cfb26b78ca88547b674bd05376fa236673b5ce5
Loch32 <loch932@gmail.com>
2023-08-15 14:07:40 +0800

    Update converter (0.2).py

--- converter (0.2).py
+++ converter (0.2).py
@@ -6,7 +6,12 @@ IntCurrency = input("Please choose the initial currency, using a three-letter IS
 
 if IntCurrency == "AUD":
         IntCurAmount = int(input("Please enter the initial currency amount as a decimal: "))
-elif IntCurrency == "help".lower: print("https://en.wikipedia.org/wiki/ISO_4217#List_of_ISO_4217_currency_codes")
+elif IntCurrency == "help":
+      print("For a list of currency codes visit: https://en.wikipedia.org/wiki/ISO_4217#List_of_ISO_4217_currency_codes")
 else: print("Please try a different country code. At the moment, this converter only supports AUD, EUR, GBP, and USD.")
 
 TargCurrency = input("Please choose the target currency, using a three-letter ISO 4217 currency code: ")
+if TargCurrency == "GBP":
+      result = IntCurAmount * 0.65
+      print(IntCurAmount, IntCurrency, "is equal to ", result, TargCurrency)
+else: print("Please try again.")   
