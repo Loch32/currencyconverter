4bb1169cbadc0fd62cc0692a1031a3e3394570e3
Loch32 <loch932@gmail.com>
2023-08-22 13:59:34 +0800

    fixed "help" infinite loop/bricking

--- converter (0.2).py
+++ converter (0.2).py
@@ -3,7 +3,7 @@
 IntCurrency = input("Please choose the initial currency, using a three-letter ISO 4217 currency code. If you are unsure of what code your currency is, type 'help': ")
 
 if IntCurrency == "AUD":
-        IntCurAmount = int(input("Please enter the initial currency amount as a decimal: ")).upper()
+        IntCurAmount = int(input("Please enter the initial currency amount as a decimal: "))
 elif IntCurrency == "help":
       print("For a list of currency codes visit: https://en.wikipedia.org/wiki/ISO_4217#List_of_ISO_4217_currency_codes")
       #typing "help" prints this string, linking to the wikipedia article that contains the correct currency codes.
--- converter (0.3).py
+++ converter (0.3).py
@@ -2,17 +2,21 @@
 
 from currency_converter import CurrencyConverter
 
-IntCurrency = input("Please choose the initial currency, using a three-letter ISO 4217 currency code. If you are unsure of what code your currency is, type 'help': ")
+IntCurrency = input("Please choose the initial currency, using a three-letter ISO 4217 currency code. If you are unsure of what code your currency is, type 'help': ").upper()
 
 if IntCurrency == "AUD":
-        IntCurAmount = int(input("Please enter the initial currency amount as a decimal: "))
-elif IntCurrency == "help":
+        IntCurAmount = float(input("Please enter the initial currency amount as a decimal: "))
+elif IntCurrency == "HELP":
       print("For a list of currency codes visit: https://en.wikipedia.org/wiki/ISO_4217#List_of_ISO_4217_currency_codes")
       #typing "help" prints this string, linking to the wikipedia article that contains the correct currency codes.
+      IntCurrency = input("Please choose the initial currency, using a three-letter ISO 4217 currency code: ").upper()
+      IntCurAmount = float(input("Please enter the initial currency amount as a decimal: "))
 else: print("Please try a different country code. At the moment, this converter only supports AUD, EUR, GBP, and USD.")
+# this code ^^^^^^^^^ (lines 7-14) were fixed by @Blep-JurassicJelly
 
-TargCurrency = input("Please choose the target currency, using a three-letter ISO 4217 currency code: ")
+TargCurrency = input("Please choose the target currency, using a three-letter ISO 4217 currency code: ").upper()
 if TargCurrency == "GBP":
       result = IntCurAmount * 0.5
+       #i havent worked out how to use any apis or libraries yet so this is using a fixed exchange rate as of 16/08/23
       print(IntCurAmount, IntCurrency, "is equal to", result, TargCurrency)
 else: print("Please try again.")
